21f5e300b99de21095fff4812e079ff5842e5376
yk <yefim_korshever@mail.ru>
2022-08-24 22:08:15 +0300

    Add shot animation

--- game.py
+++ game.py
@@ -4,6 +4,42 @@ import random
 import time
 
 
+async def fire(
+    canvas,
+    start_row,
+    start_column,
+    rows_speed=-0.3,
+    columns_speed=0
+):
+    """Display animation of gun shot, direction and speed can be specified."""
+
+    row, column = start_row, start_column
+
+    canvas.addstr(round(row), round(column), '*')
+    await asyncio.sleep(0)
+
+    canvas.addstr(round(row), round(column), 'O')
+    await asyncio.sleep(0)
+    canvas.addstr(round(row), round(column), ' ')
+
+    row += rows_speed
+    column += columns_speed
+
+    symbol = '-' if columns_speed else '|'
+
+    rows, columns = canvas.getmaxyx()
+    max_row, max_column = rows - 1, columns - 1
+
+    curses.beep()
+
+    while 0 < row < max_row and 0 < column < max_column:
+        canvas.addstr(round(row), round(column), symbol)
+        await asyncio.sleep(0)
+        canvas.addstr(round(row), round(column), ' ')
+        row += rows_speed
+        column += columns_speed
+
+
 async def blink(canvas, row, column, symbol='*'):
     while True:
         canvas.addstr(row, column, symbol, curses.A_DIM)
@@ -34,16 +70,22 @@ def draw(canvas):
     stars_number = random.randint(min_stars_number, max_stars_number)
     max_row -= 2
     max_column -= 2
-    corutines = []
+    coroutines = []
+    coroutines.append(fire(canvas, max_row // 2, max_column // 2, -1))
     for __ in range(stars_number):
         row = random.randint(1, max_row)
         column = random.randint(1, max_column)
         symbol = random.choice('+*.:')
-        corutines.append(blink(canvas, row, column, symbol))
+        coroutines.append(blink(canvas, row, column, symbol))
 
     while True:
-        for corutine in corutines:
-            corutine.send(None)
+        for coroutine in coroutines.copy():
+            try:
+                coroutine.send(None)
+            except StopIteration:
+                coroutines.remove(coroutine)
+        if len(coroutines) == 0:
+            break
         canvas.refresh()
         time.sleep(0.1)
 
