e0e89a956739d3da69d490bacfdc2371146f0ab6
yk <yefim_korshever@mail.ru>
2022-09-10 11:11:53 +0300

    Add frames to trash

--- game.py
+++ game.py
@@ -7,88 +7,11 @@ import statistics
 import time
 
 from physics import update_speed
+from curses_tools import get_frame_size, read_controls, draw_frame
+from obstacles import Obstacle, show_obstacles
 
-SPACE_KEY_CODE = 32
-LEFT_KEY_CODE = 260
-RIGHT_KEY_CODE = 261
-UP_KEY_CODE = 259
-DOWN_KEY_CODE = 258
 coroutines = []
-
-
-def get_frame_size(text):
-    """Calculate size of multiline text fragment,
-    return pair — number of rows and colums."""
-
-    lines = text.splitlines()
-    rows = len(lines)
-    columns = max([len(line) for line in lines])
-    return rows, columns
-
-
-def read_controls(canvas):
-    """Read keys pressed and returns tuple witl controls state."""
-
-    rows_direction = columns_direction = 0
-    space_pressed = False
-
-    while True:
-        pressed_key_code = canvas.getch()
-
-        if pressed_key_code == -1:
-            # https://docs.python.org/3/library/curses.html#curses.window.getch
-            break
-
-        if pressed_key_code == UP_KEY_CODE:
-            rows_direction = -1
-
-        if pressed_key_code == DOWN_KEY_CODE:
-            rows_direction = 1
-
-        if pressed_key_code == RIGHT_KEY_CODE:
-            columns_direction = 1
-
-        if pressed_key_code == LEFT_KEY_CODE:
-            columns_direction = -1
-
-        if pressed_key_code == SPACE_KEY_CODE:
-            space_pressed = True
-
-    return rows_direction, columns_direction, space_pressed
-
-
-def draw_frame(canvas, start_row, start_column, text, negative=False):
-    """Draw multiline text fragment on canvas, erase text instead of drawing
-    if negative=True is specified."""
-
-    rows_number, columns_number = canvas.getmaxyx()
-
-    for row, line in enumerate(text.splitlines(), round(start_row)):
-        if row < 0:
-            continue
-
-        if row >= rows_number:
-            break
-
-        for column, symbol in enumerate(line, round(start_column)):
-            if column < 0:
-                continue
-
-            if column >= columns_number:
-                break
-
-            if symbol == ' ':
-                continue
-
-            # Check that current position it is not in a lower right corner of
-            # the window
-            # Curses will raise exception in that case. Don`t ask why…
-            # https://docs.python.org/3/library/curses.html#curses.window.addch
-            if row == rows_number - 1 and column == columns_number - 1:
-                continue
-
-            symbol = symbol if not negative else ' '
-            canvas.addch(row, column, symbol)
+obstacles = []
 
 
 async def sleep(tics=1):
@@ -111,13 +34,16 @@ async def fly_garbage(canvas, column, garbage_frame, speed=0.5):
 
     column = max(column, 0)
     column = min(column, columns_number - 1)
-
+    rows_size, columns_size = get_frame_size(garbage_frame)
     row = 0
 
     while row < rows_number:
         draw_frame(canvas, row, column, garbage_frame)
+        obstacle = Obstacle(row, column, rows_size, columns_size)
+        obstacles.append(obstacle)
         await asyncio.sleep(0)
         draw_frame(canvas, row, column, garbage_frame, negative=True)
+        obstacles.remove(obstacle)
         row += speed
 
 
@@ -291,6 +217,8 @@ def draw(canvas):
     coroutines.append(
         fill_orbit_with_garbage(canvas, trash_frames, max_column)
     )
+
+    coroutines.append(show_obstacles(canvas, obstacles))
 
     while True:
         for coroutine in coroutines.copy():
